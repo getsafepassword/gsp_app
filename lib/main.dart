8e5540b87b7cb5704dce3ec70707123aa3149f97
AMF <work@afdesk.com>
2021-04-29 20:05:13 +0600

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: "GetSafe Password App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("GetSafePassword Title"),
+      ),
+      child: Text("GetSafePassword"),
+    );
+  }
+}
